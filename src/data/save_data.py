cceee23a27fd2a8272d15103b61bff7a1f39a07b
alexandrasmith <alexandrasmith0904@gmail.com>
2023-02-09 15:01:03 +0200

    function to save processed data as .pt

---
+++ src/data/save_data.py
@@ -0,0 +1,22 @@
+import pickle
+import torch
+import sys
+sys.path.append('/Users/alexandrasmith/Desktop/Workspace/Projects/masters/src/')
+from data.preprocessing import *
+
+SVS_DIR='/Users/alexandrasmith/Desktop/Workspace/Projects/masters/data/raw/svs_files/'
+MASK_DIR='/Users/alexandrasmith/Desktop/Workspace/Projects/masters/data/interim/masks/'
+
+# Define variables
+PATCH_SIZE=299
+STRIDE=PATCH_SIZE ### non-overlapping patches
+
+NUM_CLASSES=2
+
+# Your preprocessed tensor
+patches, labels = load_data(SVS_DIR, MASK_DIR, PATCH_SIZE, STRIDE, NUM_CLASSES)
+
+# Save data
+SAVE_DEST = '/Users/alexandrasmith/Desktop/Workspace/Projects/masters/data/processed/tests/'
+torch.save(patches, SAVE_DEST + '00-patches.pt')
+torch.save(labels, SAVE_DEST + '00-labels.pt')
