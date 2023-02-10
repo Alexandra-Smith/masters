6079048d483fef3fdc1263cc5d5ac45c3d3635d2
alexandrasmith <alexandrasmith0904@gmail.com>
2023-02-10 19:07:13 +0200

    updated pretrained inceptionv3 to download weights

--- src/models/initialise_models.py
+++ src/models/initialise_models.py
@@ -3,12 +3,13 @@ import torch.nn as nn
 import torch.optim as optim
 import torchvision.models as models
 
-def initialise_inceptionv3_model(num_classes: int, learning_rate, momentum):
+def initialise_inceptionv3_model(num_classes: int, learning_rate: float, epsilon: float, weight_decay: float, momentum: float):
     '''
     Function to initialise the inceptionv3 model
     '''
     # Define model
-    model = models.inception_v3(pretrained=True)
+    # model = models.inception_v3(pretrained=True)
+    model = torch.hub.load('pytorch/vision', 'inceptionV3', pretrained=True)
 
     # Modify final fully connected layer to have 'num_classes' output classes
     model.fc = nn.Linear(model.fc.in_features, num_classes)
@@ -19,7 +20,7 @@ def initialise_inceptionv3_model(num_classes: int, learning_rate, momentum):
     model.fc.requires_grad = True
 
     # Define the loss function and optimizer
-    loss_func = nn.BCELoss()
+    loss_func = nn.CrossEntropyLoss()
     optimiser = optim.SGD(model.fc.parameters(), lr=learning_rate, momentum=momentum)
 
     return model, loss_func, optimiser
