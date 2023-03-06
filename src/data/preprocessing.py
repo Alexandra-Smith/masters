d36efef302f91af4b55d32b6d14df334167043ea
alexandrasmith <alexandrasmith0904@gmail.com>
2023-03-06 15:20:18 +0200

    troubleshooting memory problem

--- src/data/preprocessing.py
+++ src/data/preprocessing.py
@@ -6,6 +6,78 @@ from openslide import open_slide
 from PIL import Image
 Image.MAX_IMAGE_PIXELS = None
 
+def LOAD(case_code, images_directory, gt_directory, patch_size: int, stride: int, num_classes):
+    '''
+    Function to load in ONE image, apply preprocessing steps,
+    extract patches and get the corresponing patch labels.
+    Also captures specific information about each slide, to be written to a database.
+
+    Returns:
+    patches: all patches extracted from the image that contain sufficient amount of tissue
+    labels: corresponding class labels for all patches
+    '''
+
+    # LOAD SVS FILE
+    file = images_directory + case_code + '.svs'
+    sld = open_slide(file)
+    slide_props = sld.properties
+    slide_width = int(slide_props['openslide.level[1].width']); slide_height = int(slide_props['openslide.level[1].height']) # dimensions at 10X magnification
+    slide = np.array(sld.get_thumbnail(size=(slide_width, slide_height)))
+
+    # # LOAD SEGMENTATION MASK
+    # mask_file = gt_directory + case_code + '.png'
+    # mask = np.array(Image.open(mask_file))
+    # mask = mask[:slide_height, :slide_width] # reshape mask file to be same size as SVS
+
+    # if slide_height != mask.shape[0] or slide_width != mask.shape[1]:
+    #     raise Exception("Input SVS file and segmentation image do not have the same dimensions")
+
+    # extract patches
+    patches = image_to_patches(slide, patch_size, stride)
+    # create patches for segmentation masks
+    # mask_patches = image_to_patches(mask, patch_size, stride)
+    # # get rid of background patches
+    # tissue_patches, gt_patches = discard_background_patches(patches, mask_patches, patch_size)
+
+    # # Get labels
+    # labels = get_patch_labels(gt_patches, patch_size)
+
+    # create dataframe to capture data information
+    # df_data = []
+    # col_names = ['File_name', 'Level0_factor', 'Level1_factor', 'Level2_factor', 'Level3_factor', 'Level0_height', 'Level0_width',
+    #             'Level1_height', 'Level1_width', 'Level2_height', 'Level2_width', 'Level3_height', 'Level3_width',
+    #             'Patch_size', 'Stride', 'Total_num_of_patches', 'Num_of_patches_discarded', '%_of_benign_tiles','%_of_tumourous_tiles']
+    # levels = len(sld.level_dimensions) # get num of levels
+    # if levels > 4:
+    #     raise Exception("More than 4 levels contained in this svs file")
+    # factors = np.array(sld.level_downsamples)
+    # # start row
+    # data = [case_code.split('.')[0]] # first entry is the file name
+    # for r in range(levels):
+    #     data.append(factors[r]) # add all downsample factors for each level
+    # if levels < 4:
+    #     data.append(np.nan)
+    # total_num_patches = patches.shape[0]
+    # num_discarded_patches = total_num_patches - tissue_patches.shape[0]
+    # percentage_benign = (torch.sum(torch.eq(labels, 0))/len(labels)).item() * 100
+    # percentage_tumourous = (torch.sum(torch.eq(labels, 1))/len(labels)).item() * 100
+    # if levels < 4:
+    #     data.extend([slide_props['openslide.level[0].height'], slide_props['openslide.level[0].width'], 
+    #             slide_props['openslide.level[1].height'], slide_props['openslide.level[1].width'],
+    #             slide_props['openslide.level[2].height'], slide_props['openslide.level[2].width'],
+    #             np.nan, np.nan, patch_size, stride, total_num_patches, num_discarded_patches, percentage_benign, percentage_tumourous])
+    # else:
+    #     data.extend([slide_props['openslide.level[0].height'], slide_props['openslide.level[0].width'], 
+    #             slide_props['openslide.level[1].height'], slide_props['openslide.level[1].width'],
+    #             slide_props['openslide.level[2].height'], slide_props['openslide.level[2].width'],
+    #             slide_props['openslide.level[3].height'], slide_props['openslide.level[3].width'],
+    #             patch_size, stride, total_num_patches, num_discarded_patches, percentage_benign, percentage_tumourous])
+    # df_data.append(data)
+    # df = pd.DataFrame(df_data, columns=col_names)
+    
+    # return tissue_patches, labels, df
+    return patches
+
 def load_indv_case(case_code, images_directory, gt_directory, patch_size: int, stride: int, num_classes):
     '''
     Function to load in ONE image, apply preprocessing steps,
@@ -55,11 +127,19 @@ def load_indv_case(case_code, images_directory, gt_directory, patch_size: int, s
     data = [case_code.split('.')[0]] # first entry is the file name
     for r in range(levels):
         data.append(factors[r]) # add all downsample factors for each level
+    if levels < 4:
+        data.append(np.nan)
     total_num_patches = patches.shape[0]
     num_discarded_patches = total_num_patches - tissue_patches.shape[0]
     percentage_benign = (torch.sum(torch.eq(labels, 0))/len(labels)).item() * 100
     percentage_tumourous = (torch.sum(torch.eq(labels, 1))/len(labels)).item() * 100
-    data.extend([slide_props['openslide.level[0].height'], slide_props['openslide.level[0].width'], 
+    if levels < 4:
+        data.extend([slide_props['openslide.level[0].height'], slide_props['openslide.level[0].width'], 
+                slide_props['openslide.level[1].height'], slide_props['openslide.level[1].width'],
+                slide_props['openslide.level[2].height'], slide_props['openslide.level[2].width'],
+                np.nan, np.nan, patch_size, stride, total_num_patches, num_discarded_patches, percentage_benign, percentage_tumourous])
+    else:
+        data.extend([slide_props['openslide.level[0].height'], slide_props['openslide.level[0].width'], 
                 slide_props['openslide.level[1].height'], slide_props['openslide.level[1].width'],
                 slide_props['openslide.level[2].height'], slide_props['openslide.level[2].width'],
                 slide_props['openslide.level[3].height'], slide_props['openslide.level[3].width'],
@@ -168,7 +248,7 @@ def image_to_patches(image, patch_size: int, stride: int):
 
     # Is image colour or binary?
     image_dimension = 3 if len(image.shape) == 3 else 1
-
+    print("Extracting patches")
     # Working with a colour image
     if image_dimension == 3:
         # Extract patches
@@ -207,37 +287,39 @@ def discard_background_patches(svs_patches, mask_patches, patch_size: int):
     tissue_patches = torch.empty((0, 3, patch_size, patch_size))
     seg_patches = torch.empty((0, patch_size, patch_size))
 
-    # Method from Coudray et al.
-    print(f"Total patches: {total_num_patches}")
-    for i in range(total_num_patches):
-        # Get patch (torch tensor)
-        im_patch = svs_patches[i, :, :, :]
-        mask_patch = mask_patches[i, :, :]
-        # Convert to numpy array
-        p = im_patch.numpy()
-        # Determine slides with low amount of information (>50% covered in background)
-        # i.e. all values below 210 in RGB colour space
-        avg = np.average(p)*255
-        if avg < 210:
-            tissue_patches = torch.cat((tissue_patches, im_patch.unsqueeze(0)), dim=0)
-            seg_patches = torch.cat((seg_patches, mask_patch.unsqueeze(0)), dim=0)
-        if i%100==0: print(f"{i}/{total_num_patches}") 
-
-    # FIRST METHOD TRIED
+    # # Method from Coudray et al.?
+    # print(f"Total patches: {total_num_patches}")
     # for i in range(total_num_patches):
     #     # Get patch (torch tensor)
     #     im_patch = svs_patches[i, :, :, :]
     #     mask_patch = mask_patches[i, :, :]
     #     # Convert to numpy array
-    #     p = mask_patch.numpy()
-    #     # Number of black pixels
-    #     nblack_px = np.sum(p == 0)
-    #     # Calculate % of background (black) pixels
-    #     background_percentage = nblack_px/total_px
-    #     # Keep patch if background < 80% of the patch (i.e patch contains > 80% tissue), otherwise discard it
-    #     if background_percentage < 0.8:
+    #     p = im_patch.numpy()
+    #     # Determine slides with low amount of information (>50% covered in background)
+    #     # i.e. all values below 210 in RGB colour space
+    #     avg = np.average(p)*255
+    #     if avg < 210:
     #         tissue_patches = torch.cat((tissue_patches, im_patch.unsqueeze(0)), dim=0)
     #         seg_patches = torch.cat((seg_patches, mask_patch.unsqueeze(0)), dim=0)
+
+    # FIRST METHOD TRIED
+    for i in range(total_num_patches):
+        # Get patch (torch tensor)
+        im_patch = svs_patches[i, :, :, :]
+        mask_patch = mask_patches[i, :, :]
+        # Convert to numpy array
+        p = mask_patch.numpy()
+        # Count number of pixels in different classes
+        # nblack_px = np.sum(p == 0) 
+        nwhite_px = np.sum(p == 1); ngrey_px = np.sum(p == 0.5)
+        # Calculate % of background (black) pixels
+        # background_percentage = nblack_px/total_px
+        tissue_percentage = (ngrey_px + nwhite_px)/total_px
+        # Keep patch if background < 70% of the patch (i.e patch contains > 70% tissue), otherwise discard it
+        if tissue_percentage > 0.7:
+            tissue_patches = torch.cat((tissue_patches, im_patch.unsqueeze(0)), dim=0)
+            seg_patches = torch.cat((seg_patches, mask_patch.unsqueeze(0)), dim=0)
+        if i%1000==0: print(f"{i}/{total_num_patches}") 
 
     return tissue_patches, seg_patches
 
@@ -265,8 +347,8 @@ def get_patch_labels(patches, patch_size):
         nwhite_px = np.sum(p == 1)
         # Calculate % of white pixels == tumourous pixels
         tumour_percentage = nwhite_px/total_px
-        # Patch is considered to be labelled tumourous (1) if > 80% of the tissue in the patch is tumourous, otherwise it is labelled as normal tissue (0)
-        if tumour_percentage > 0.8:
+        # Patch is considered to be labelled tumourous (1) if > 70% of the tissue in the patch is tumourous, otherwise it is labelled as normal tissue (0)
+        if tumour_percentage > 0.5:
             labels.append(1)
         else:
             labels.append(0)
--- src/data/save_data.py
+++ src/data/save_data.py
@@ -20,31 +20,33 @@ for file in files:
     file_codes.append(name)
 
 # Define variables
-PATCH_SIZE=299
+PATCH_SIZE=256
 STRIDE=PATCH_SIZE ### non-overlapping patches
 
 NUM_CLASSES=2
 
 # since = time.time()
 
-i = 1
+# i = 1
 
-case_code = file_codes[i]
+# case_code = file_codes[i]
+case_code = 'TCGA-D8-A1J9-01Z-00-DX1.F81FA9EF-8129-4E17-A9AD-2B850782CC18'
 # Preprocess data
-patches, labels, df = load_indv_case(case_code, SVS_DIR, MASK_DIR, PATCH_SIZE, STRIDE, NUM_CLASSES)
+# patches, labels, df = load_indv_case(case_code, SVS_DIR, MASK_DIR, PATCH_SIZE, STRIDE, NUM_CLASSES)
+patches = LOAD(case_code, SVS_DIR, MASK_DIR, PATCH_SIZE, STRIDE, NUM_CLASSES)
 
 # Save data
-SAVE_DEST = '/Users/alexandrasmith/Desktop/Workspace/Projects/masters/data/processed/'
-torch.save(patches, SAVE_DEST + 'patches/' + case_code.split('.')[0] + '.pt')
-torch.save(labels, SAVE_DEST + 'labels/' + case_code.split('.')[0] + '.pt')
+# SAVE_DEST = '/Volumes/AlexS/MastersData/processed/'
+# SAVE_DEST__ = '/Users/alexandrasmith/Desktop/Workspace/Projects/masters/data/processed/'
+# torch.save(patches, SAVE_DEST + 'patches/' + case_code.split('.')[0] + '.pt')
+# torch.save(labels, SAVE_DEST + 'labels/' + case_code.split('.')[0] + '.pt')
 
-# export data to csv file (do for first file)
-# df.to_csv(SAVE_DEST + 'data_info.csv', float_format='%.8f')  # index=False means not to write row numbers
+# # export data to csv file (do for first file)
+# df.to_csv(SAVE_DEST__ + 'data_info.csv', float_format='%.8f')  # index=False means not to write row numbers
 
 # read in csv file and append new data (rows)
-df.to_csv(SAVE_DEST + 'data_info.csv', mode='a', header=False, float_format='%.8f')
+# df.to_csv(SAVE_DEST__ + 'data_info.csv', mode='a', header=False, float_format='%.8f')
 
-# ! print out some example patches and their labels
 
 # time_elapsed = time.time() - since
 # print('Complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
