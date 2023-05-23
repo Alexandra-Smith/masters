e48a2dd5ce9c1a6cfedddcad5f3065a810a494ff
alexandrasmith <alexandrasmith0904@gmail.com>
2023-05-23 15:38:02 +0200

    model testing script

---
+++ src/models/model_testing.py
@@ -0,0 +1,334 @@
+'''
+RUN SCRIPT AS:
+model_testing.py path_to_model_weights
+'''
+
+from tqdm import tqdm
+import os
+import sys
+import torch
+import torch.nn as nn
+from torchvision import transforms
+from torch.utils.data import Dataset
+import random
+import torch.utils.data as data_utils
+from PIL import Image
+from sklearn import metrics
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_auc_score, roc_curve, confusion_matrix, ConfusionMatrixDisplay
+
+def load_trained_model(num_classes, model_path): 
+
+    # # INCEPTION
+    # # Define model architecture
+    # model = torch.hub.load('pytorch/vision:v0.6.0', 'inception_v3', pretrained=True)
+    # # Replace last layers with new layers
+    # num_ftrs = model.fc.in_features
+    # model.fc = nn.Sequential(
+    #     nn.Linear(num_ftrs, 2048),
+    #     nn.ReLU(inplace=True),
+    #     nn.Dropout(p=0.7),
+    #     nn.Linear(2048, num_classes),
+    #     nn.Softmax(dim=1)
+    # )
+    model = nn.Sequential(
+        nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=2),
+        nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=2),
+        nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=2),
+        nn.Flatten(),
+        nn.Linear(64 * 32 * 32, 512),
+        nn.ReLU(),
+        nn.Linear(512, num_classes),
+    )
+
+    # Load the saved model state dict
+    model.load_state_dict(torch.load(model_path))
+
+    # Set the model to evaluation mode
+    model.eval()
+
+    return model
+
+def test_model(model, test_loader, device):
+    
+    correct = 0
+    total = 0
+    
+    # Create a progress bar
+    progress_bar = tqdm(test_loader, desc='Testing', unit='batch')
+
+    with torch.no_grad():
+        true_labels = []
+        predictions = []
+        probabilities = []
+        for inputs, labels in progress_bar:
+            # move to device
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            true_labels.extend(labels.tolist())
+
+            # Forward pass
+            outputs = model(inputs)
+            probs = torch.softmax(outputs, dim=1)
+            # Get predicted labels
+            _, predicted = torch.max(outputs.data, 1)
+
+            # Update variables
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+            probabilities.extend(probs.tolist())
+            predictions.extend(predicted.tolist())
+
+            # Update progress bar description
+            progress_bar.set_postfix({'Accuracy': '{:.2f}%'.format((correct / total) * 100)})
+    
+    # # Compute accuracy (testing for now) --delete
+    accuracy = 100 * correct / total
+    print('Test Accuracy: {:.2f}%'.format(accuracy))
+    # Close the progress bar
+    progress_bar.close()
+    
+    return true_labels, probabilities, predictions
+
+# Split image folders into train, val, test
+def split_data(patch_directory, split: list, seed):
+    '''
+    Function that takes in the split percentage for train/val/test sets, and randomly chooses which cases
+    to allocate to which set (to ensure all patches from one case go into one set)
+    Parameters:
+    patch_directory: folder containing all patches
+    split: list of integers for splitting sets
+    seed: option to set the seed value for randomness
+    Returns:
+    3 lists for each of train/val/test, where each list contains the case names to be used in the set
+    '''
+    
+    random.seed(seed)
+
+    case_folders = os.listdir(patch_directory) # get 147 case folders
+    
+    d = {}
+    for folder in case_folders:
+        num_patches_in_folder = len(os.listdir(patch_directory + folder))
+        d[folder] = num_patches_in_folder
+    
+    total_num_patches = sum(d.values())
+    train_split, val_split, test_split = split
+    train_num_patches = int((train_split/100)*total_num_patches)
+    val_num_patches = int((val_split/100)*total_num_patches)
+
+    # list all folders in the directory
+    folders = [os.path.join(patch_directory, folder) for folder in os.listdir(patch_directory) if os.path.isdir(os.path.join(patch_directory, folder))]
+    
+    # SELECT TRAINING CASES
+    train_cases = [] # store all selected cases
+    num_selected_train = 0 # number of patches selected so far
+    selected_folders = set() # a set to store the selected folder names to keep track of those already selected
+    while num_selected_train < train_num_patches:
+        folder = random.choice(folders)
+        if folder not in selected_folders:
+            case = folder.replace(patch_directory, '')
+            num_patches = len(os.listdir(folder))
+            num_selected_train += num_patches
+            selected_folders.add(folder) # add to set of selected folders
+            train_cases.append(case)
+
+    # SELECT VAL CASES
+    val_cases = [] # store all selected cases
+    num_selected_val = 0 # number of patches selected so far
+    while num_selected_val < val_num_patches:
+        folder = random.choice(folders)
+        if folder not in selected_folders:
+            case = folder.replace(patch_directory, '')
+            num_patches = len(os.listdir(folder))
+            num_selected_val += num_patches
+            selected_folders.add(folder)
+            val_cases.append(case)
+
+    # SELECT TEST CASES
+    cases = [folder.replace(patch_directory, '') for folder in folders]
+    used = train_cases+val_cases
+    test_cases = [case for case in cases if case not in used]
+    
+    # test_patches = [len(os.listdir(patch_directory + folder)) for folder in test_cases]
+    num_selected_test = sum([len(os.listdir(patch_directory + folder)) for folder in test_cases])
+    # dict = {x: for x in ['train', 'val', 'test']}
+    print(f"Number of training patches: {num_selected_train} \nNumber of validation patches {num_selected_val} \nNumber of test patches {num_selected_test}")
+    return train_cases, val_cases, test_cases
+
+# Create a custom PyTorch dataset to read in your images and apply transforms
+
+class CustomDataset(Dataset):
+    def __init__(self, img_folders, label_files, transform=None):
+        self.img_folders = img_folders
+        self.label_files = label_files
+        self.transform = transform
+
+        self.imgs = [] # Keeps image paths to load in the __getitem__ method
+        self.labels = []
+
+        # Load images and corresponding labels
+        for i, (img_folder, label_file) in enumerate(zip(img_folders, label_files)):
+            # print("Patch directory", img_folder, "\nLabel file", label_file)
+            labels_pt = torch.load(label_file) # Load .pt file
+            # Run through all patches from the case folder
+            for i, img in enumerate(os.listdir(img_folder)):
+                if os.path.isfile(img_folder + '/' + img) and os.path.isfile(label_file):
+                    # print(img_folder + img)
+                    if img.startswith('._'):
+                        img = img.replace('._', '')
+                    idx = int(img.replace('.png', '').split("_")[1])
+                    self.imgs.append(img_folder + '/' + img)
+                    self.labels.append(labels_pt[idx].item()) # get label as int
+        
+    def __len__(self):
+        return len(self.imgs)
+    
+    def __getitem__(self, idx):
+        # Load image at given index
+        image_path = self.imgs[idx]
+        image = Image.open(image_path).convert('RGB')
+        
+        if self.transform is not None: # Apply transformations
+            image = self.transform(image)
+        
+        label = self.labels[idx] # Load corresponding image label
+        
+        return image, label # Return transformed image and label
+    
+def load_data(INPUT_SIZE, SEED, batch_size, num_cpus):
+    # DATALOADERS
+
+    # Initialise data transforms
+    data_transforms = {
+        'train': transforms.Compose([
+            transforms.Resize(INPUT_SIZE),
+            # transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+        'val': transforms.Compose([
+            transforms.Resize(INPUT_SIZE),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+        'test' : transforms.Compose([
+            transforms.Resize(INPUT_SIZE),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+    }
+
+    # using full set of data
+    # img_dir = '../data/processed/patches/'
+    # labels_dir = '../data/processed/labels/'
+    img_dir = '/Volumes/AlexS/MastersData/processed/patches/'
+    labels_dir = '/Volumes/AlexS/MastersData/processed/labels/'
+
+    split=[70, 15, 15] # for splitting into train/val/test
+
+    train_cases, val_cases, test_cases = split_data(img_dir, split, SEED)
+
+    train_img_folders = [img_dir + case for case in train_cases]
+    val_img_folders = [img_dir + case for case in val_cases]
+    test_img_folders = [img_dir + case for case in test_cases]
+
+    # Contains the file path for each .pt file for the cases used in each of the sets
+    train_labels = [labels_dir + case + '.pt' for case in train_cases]
+    val_labels = [labels_dir + case + '.pt' for case in val_cases]
+    test_labels = [labels_dir + case + '.pt' for case in test_cases]
+
+    image_datasets = {
+        'train': CustomDataset(train_img_folders, train_labels, transform=data_transforms['train']),
+        'val': CustomDataset(val_img_folders, val_labels, transform=data_transforms['val']),
+        'test': CustomDataset(test_img_folders, test_labels, transform=data_transforms['test'])
+    }
+    # Create training, validation and test dataloaders
+    dataloaders = {
+        'train': data_utils.DataLoader(image_datasets['train'], batch_size=batch_size, num_workers=num_cpus, shuffle=True, drop_last=True),
+        'val': data_utils.DataLoader(image_datasets['val'], batch_size=batch_size, num_workers=num_cpus, shuffle=True),
+        'test': data_utils.DataLoader(image_datasets['test'], batch_size=batch_size, num_workers=num_cpus, shuffle=True)
+    }
+
+    return dataloaders
+
+def roc_plot(y_test, predicted_probs):
+    # keep probabilities for the positive outcome only
+    predicted_probs = predicted_probs[:, 1]
+    # calculate scores
+    auc_score = roc_auc_score(y_test, predicted_probs)
+    # summarize scores
+    print('ROC AUC=%.3f' % (auc_score))
+    # calculate roc curves
+    fpr, tpr, _ = roc_curve(y_test, predicted_probs)
+    # generate a no skill prediction (majority class)
+    ns_probs = [0 for _ in range(len(y_test))]
+    ns_fpr, ns_tpr, _ = roc_curve(y_test, ns_probs)
+
+    # plot the roc curve for the model
+    plt.plot(ns_fpr, ns_tpr, linestyle='--', label='No Skill')
+    plt.plot(fpr, tpr, marker='.', label='Model')
+    # axis labels
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    # show the legend
+    plt.legend()
+    # show the plot
+    plt.show()
+
+def get_metrics(y_test, predictions):
+    report = metrics.classification_report(y_test, predictions)
+    return report
+
+def plot_confusion_matrix(y_test, predictions):
+    cm = confusion_matrix(y_test, predictions)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+    disp.plot()
+    plt.show()
+
+def main():
+    ##### SET PARAMETERS #####
+    num_classes = 2
+    batch_size = 64
+    model_name = 'None'
+
+    PATCH_SIZE=256
+    STRIDE=PATCH_SIZE
+    SEED=42
+    num_cpus=8
+
+    if model_name == 'inception': 
+        INPUT_SIZE=299 
+    else: INPUT_SIZE=PATCH_SIZE
+
+    # Load data
+    dataloaders = load_data(INPUT_SIZE, SEED, batch_size, num_cpus)
+
+    # Load model
+    model_path = sys.argv[1]
+    model = load_trained_model(num_classes, model_path)
+
+    # Detect if we have a GPU available
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    # Send the model to GPU
+    model = model.to(device)
+
+    true_labels, model_probabilities, model_predictions = test_model(model, dataloaders['test'], device)
+
+    metrics = get_metrics(true_labels, model_predictions)
+    print(metrics)
+
+    roc_plot(true_labels, model_probabilities)
+
+    plot_confusion_matrix(true_labels, model_probabilities)
+
+
+if __name__ == '__main__':
+    main()
